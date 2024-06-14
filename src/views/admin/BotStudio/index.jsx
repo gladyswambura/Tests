a6cdfba0a1515a4ab09498be8b03d1363b733755
MaureenWanjiku <nyingimaureen19@gmail.com>
2024-06-14 10:20:35 +0300

    added menus to the tabs

--- src/views/admin/BotStudio/index.jsx
+++ src/views/admin/BotStudio/index.jsx
@@ -6,7 +6,7 @@ import Widget from "components/widget/Widget";
 import AddNodeForm from '../../../components/nodes/add-node';
 import AddBulkNodeForm from '../../../components/nodes/add-bulk-node';
 import ReviewNodesForm from '../../../components/nodes/review-nodes';
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import TabMenu from "./tabs";
 
 
 const BotStudio = () => {
@@ -14,16 +14,9 @@ const BotStudio = () => {
   return (
     <div>
       <div className="mt-4">
-        <Tabs variant='unstyled'>
-          <TabList className="flex justify-around text-xl">
-          <Tab _selected={{ borderBottom: '2px solid rgb(29 78 216)', color: 'blue.500' }} className="pb-2">Node Maintenance</Tab>
-            <Tab _selected={{ borderBottom: '2px solid rgb(29 78 216)', color: 'blue.500' }} className="pb-2">Parameters Configuration</Tab>
-            <Tab _selected={{ borderBottom: '2px solid rgb(29 78 216)', color: 'blue.500' }} className="pb-2">Conversation Designers</Tab>
-            <Tab _selected={{ borderBottom: '2px solid rgb(29 78 216)', color: 'blue.500' }} className="pb-2">Booking Management</Tab>
-          </TabList>
-        </Tabs>
+        <TabMenu />
       </div>
-      <div className="bg-white rounded-lg shadow-md p-6">
+      {/* <div className="bg-white rounded-lg shadow-md p-6">
         <div className="mb-12">
           <p className="mb-4 text-xl">Nodes are leaves stored in the knowledge tree. They are searched on User SMS received to reply correctly</p>
           <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-3">
@@ -119,7 +112,7 @@ const BotStudio = () => {
             </Link>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
---
+++ src/views/admin/BotStudio/tabs.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import { Tabs, TabList, Tab, Box, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import './styles.css'
+
+
+const TabMenu = () => {
+
+  const [nodeMenuOpen, setNodeMenuOpen] = useState(false)
+  const [paramsMenuOpen, setParamsMenuOpen] = useState(false)
+  const [conversationMenuOpen, setConversationMenuOpen] = useState(false)
+  const [bookingMenuOpen, setBookingMenuOpen] = useState(false)
+
+  const handleNodeMenuOpen = () => setNodeMenuOpen(true)
+  const handleNodeMenuClose = () => setNodeMenuOpen(false)
+
+  const handleParamsMenuOpen = () => setParamsMenuOpen(true)
+  const handleParamsMenuClose = () => setParamsMenuOpen(false)
+
+  const handleconversationMenuOpen = () => setConversationMenuOpen(true)
+  const handleConversationMenuClose = () => setConversationMenuOpen(false)
+
+  const handleBookingMenuOpen = () => setBookingMenuOpen(true)
+  const handleBookingMenuClose = () => setBookingMenuOpen(false)
+
+
+  return (
+    <Tabs variant="unstyled">
+      <TabList className="flex justify-around text-xl">
+        <Box p="2" rounded="md" flex="1" mx="1" onMouseEnter={handleNodeMenuOpen} onMouseLeave={handleNodeMenuClose} position="relative">
+          <Menu isOpen={nodeMenuOpen}>
+            <MenuButton as={Tab} _expanded={{ borderBottom: '2px solid rgb(29 78 216)', color: 'black' }} className="menu-button-hover" variant="unstyled">
+              Node Maintenance
+            </MenuButton>
+            <MenuList bg='white' boxShadow='xl' px='30' py='8' fontSize='1rem' className='box-shadow box-shadow-hover'>
+              <MenuItem>Add Node</MenuItem>
+              <MenuItem>Review Node</MenuItem>
+              <MenuItem>Add Bulk Nodes</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+        <Box p="2" rounded="md" flex="1" mx="1" onMouseEnter={handleParamsMenuOpen} onMouseLeave={handleParamsMenuClose} position='relative'>
+          <Menu isOpen={paramsMenuOpen}>
+            <MenuButton as={Tab} _expanded={{ borderBottom: '2px solid rgb(29 78 216)', color: 'black' }} variant="unstyled">
+              Parameters Configuration
+            </MenuButton>
+            <MenuList bg='white' boxShadow='xl' px='50' py='8' fontSize='1rem' className='box-shadow box-shadow-hover'>
+              <MenuItem>Channel Configs</MenuItem>
+              <MenuItem>Global Configs</MenuItem>
+
+            </MenuList>
+          </Menu>
+
+        </Box>
+
+        <Box p="2" rounded="md" flex="1" mx="1" onMouseEnter={handleconversationMenuOpen} onMouseLeave={handleConversationMenuClose} position='relative'>
+          <Menu isOpen={conversationMenuOpen}>
+            <MenuButton as={Tab} _expanded={{ borderBottom: '2px solid rgb(29 78 216)', color: 'black' }} variant="unstyled">
+              Conversation Designers
+            </MenuButton>
+            <MenuList bg='white' boxShadow='xl' px='50' py='8' fontSize='1rem' className='box-shadow box-shadow-hover'>
+              <MenuItem>Designer Tool</MenuItem>
+              <MenuItem>Add Conversation</MenuItem>
+              <MenuItem>Review Conversation</MenuItem>
+              <MenuItem>Templates</MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+
+        <Box p="2" rounded="md" flex="1" mx="1" onMouseEnter={handleBookingMenuOpen} onMouseLeave={handleBookingMenuClose} position='relative'>
+          <Menu isOpen={bookingMenuOpen}>
+            <MenuButton as={Tab} _expanded={{ borderBottom: '2px solid rgb(29 78 216)', color: 'black' }} variant="unstyled">
+              Booking Management
+            </MenuButton>
+            <MenuList bg='white' boxShadow='xl' px='50' py='8' fontSize='1rem' className='box-shadow box-shadow-hover'>
+              <MenuItem>Manage Users</MenuItem>
+              <MenuItem>Manage Functions</MenuItem>
+              <MenuItem>Manage Rights</MenuItem>
+              <MenuItem>Manage Bookings</MenuItem>
+            </MenuList>
+          </Menu>
+
+        </Box>
+      </TabList>
+
+    </Tabs>
+  );
+};
+
+export default TabMenu;
