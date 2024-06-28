bd52c351e0a91f7eee1db2eb47cdf66b093d156d
MaureenWanjiku <nyingimaureen19@gmail.com>
2024-06-28 10:13:29 +0300

    handling edit profile

--- src/views/admin/profile/index.jsx
+++ src/views/admin/profile/index.jsx
@@ -6,7 +6,7 @@ const ProfileOverview = () => {
   return (
     <div className="flex w-full flex-col gap-5 ">
       <div className="w-ful mt-3 flex h-fit flex-col gap-14 lg:grid lg:grid-cols-12">
-        <div className="col-span-4 lg:!mb-0">
+        <div className="col-span-4 lg:!mb-0 ml-4">
           <Banner />
         </div>
 
